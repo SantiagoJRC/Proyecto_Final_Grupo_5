a49c8468ba14ebf2df998c087031734221f257ca
Adrianap24 <adriana.pulido@est.uexternado.edu.co>
2025-11-21 05:53:08 +0000

    funcion percentiles y ultima pregunta titanic

---
+++ pandas/funciones.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def percentiles(archivo,columna):
+    try:
+        df = pd.read_csv(archivo)
+        if columna not in df.columns:
+            print("Error al procesar la columna")
+            return None
+        p25 = df[columna].quantile(0.25)
+        p50 = df[columna].quantile(0.50)
+        p75 = df[columna].quantile(0.75)
+
+        return round(p25, 2), round(p50, 2), round(p75, 2)
+
+    except:
+        j=print("Error no existe la columna")
+        return j
+
