50439e3cc6292e88ba223593add06c35fd8db1a4
tomrugle-0706 <tomas.rugeles@est.uexternado.edu.co>
2025-11-21 14:09:23 +0000

    Grafica de barras de desviaciones by (tomas)

--- pandas/funciones.py
+++ pandas/funciones.py
@@ -33,3 +33,24 @@ def promedios(n, c):
     
     except FileNotFoundError:
         print("No existe el archivo")
+
+import pandas as pd
+import numpy as np
+
+
+def desviacion(nombre_archivo, nombre_columna):
+    try:
+        try:
+            df = pd.read_csv(nombre_archivo, encoding='utf-8')
+        except:
+            df = pd.read_csv(nombre_archivo, encoding='latin-1')
+        df.columns = df.columns.str.strip()
+        if nombre_columna not in df.columns:
+            coldis = list(df.columns)
+            return f'No existe la columna: {nombre_columna}. Columnas disponibles: {coldis}'
+        desv_std = df[nombre_columna].std()
+        
+        return desv_std
+        
+    except Exception as e:
+        return f'Error: {str(e)}'
